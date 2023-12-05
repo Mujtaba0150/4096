867967571da2ecb506b8a7e7dad70b0e74a1c449
Mujtaba Ali <85437621+Mujtaba0150@users.noreply.github.com>
2023-12-05 12:29:15 +0500

    Delete Project 2048/cppgraphics-1.0.1 directory

--- Project 2048/cppgraphics-1.0.1/examples/emscripten/main.cpp
+++
@@ -1,9 +0,0 @@
-#include "cppgraphics.hpp"
-
-int main()
-{
-    cg::create_window("HELLO WORLD");
-    cg::circle(100, 100, 50);
-    cg::wait_until_closed();
-    return 0;
-}
--- Project 2048/cppgraphics-1.0.1/projects/000_template_windows_msvc2017/main.cpp
+++
@@ -1,8 +0,0 @@
-#include "cppgraphics.hpp"
-
-int main()
-{
-    cg::create_window("cppgraphics works !", 400., 300.);
-    cg::wait_until_closed();
-    return 0;
-}
